183c511f521a307a15a54ec685c43ed4fd461e2b
SameerJS6 <singh6.sameer@gmail.com>
2024-02-02 23:16:57 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const urbanist = Urbanist({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "WeFrame Assignement - Sameer Singh",
+  description: "Internship Assignement for We Frame Tech",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>{children}</body>
+    </html>
+  );
+}
