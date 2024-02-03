c9f33d6c8ea6a09b7dd22a9f4b7e26d509ab8b96
SameerJS6 <singh6.sameer@gmail.com>
2024-02-04 00:36:27 +0530

    Font Styles Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,32 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "display-2xl": ["40px", { fontWeight: 700, lineHeight: "60px" }],
+        "display-xl": ["35px", { fontWeight: 600, lineHeight: "52.5px" }],
+        "display-lg": ["30px", { fontWeight: 700, lineHeight: "36px" }],
+        "display-md": [
+          "28px",
+          {
+            fontWeight: 600,
+            lineHeight: "42px",
+          },
+        ],
+        "display-sm": ["24px", { fontWeight: 600, lineHeight: "36px" }],
+        "title-xl": ["22px", { lineHeight: "26.4px" }],
+        "title-lg": ["16px", { lineHeight: "24px" }],
+        "title-md": ["17px", { fontWeight: 500, lineHeight: "20.4px" }],
+        "label-xl": ["20px", { fontWeight: 600, lineHeight: "24px" }],
+        "label-lg": ["18px", { lineHeight: "21.6px" }],
+        "label-md": ["16px", { fontWeight: 600, lineHeight: "19.2px" }],
+        "label-sm2": ["14px", { fontWeight: 600, lineHeight: "16.8px" }],
+        "label-sm": ["14px", { fontWeight: 500, lineHeight: "19.1px" }],
+        "label-xs": ["10px", { fontWeight: 500, lineHeight: "12px" }],
+        "body-lg": ["20px", { fontWeight: 600, lineHeight: "30px" }],
+        "body-md": ["17px", { fontWeight: 400, lineHeight: "25.5px" }],
+      },
+    },
   },
   plugins: [],
 };
