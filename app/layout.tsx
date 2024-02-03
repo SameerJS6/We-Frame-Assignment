7249042244c7c2a6a956e176b7aec1a50fd34a27
SameerJS6 <singh6.sameer@gmail.com>
2024-02-04 01:31:12 +0530

    material symbols added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import "./globalicons.css";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
