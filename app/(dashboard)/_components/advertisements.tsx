64d090bb5809b0cdcdd75bf55cd7fb49bddae5bf
SameerJS6 <singh6.sameer@gmail.com>
2024-02-05 03:12:57 +0530

    minor change in advertisement section

--- app/(dashboard)/_components/advertisements.tsx
+++ app/(dashboard)/_components/advertisements.tsx
@@ -32,7 +32,7 @@ export default function Advertisements({
         <p className="text-[#A0A3BD] text-label-sm font-normal">{label}</p>
 
         <div className="flex items-center justify-between gap-3 mt-2">
-          <button className="text-[#7750F1] text-label-lg rounded-[10px] bg-[#E8E9FF] h-12 text-center flex-1">
+          <button className="text-[#7750F1] text-label-lg font-semibold rounded-[10px] bg-[#E8E9FF] h-12 text-center flex-1 transition-all duration-250 ease-in-out hover:bg-[#cfd1ff]">
             Visit
           </button>
           <button className="bg-[#FAFAFA] rounded-[10px] h-12 px-4 grid place-content-center">
