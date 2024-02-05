62c9bdee7416d71015024b52caae37896f299a4c
SameerJS6 <singh6.sameer@gmail.com>
2024-02-05 15:35:51 +0530

    images updated

--- utils/advertisementData.tsx
+++ utils/advertisementData.tsx
@@ -3,19 +3,19 @@ const AdvertisementData = [
     title: "Build your business",
     label:
       "Shopify has all the tools you need to start, sell, market, and manage",
-    image: "/icons/ad1.png",
+    image: "/images/ad1.png",
   },
   {
     title: "Libree Coffee",
     label:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-    image: "/icons/ad1.png",
+    image: "/images/ad2.png",
   },
   {
     title: "KFC",
     label:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-    image: "/icons/ad1.png",
+    image: "/images/ad3.png",
   },
 ];
 
