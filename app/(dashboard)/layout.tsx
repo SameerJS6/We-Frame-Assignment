90e04a4c8ee903e29cd874c11c0af650c6ea25cd
SameerJS6 <singh6.sameer@gmail.com>
2024-02-05 15:34:36 +0530

    layout readjusted

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from "react";
-import Sidebar from "./_components/sidebar";
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -8,8 +7,9 @@ type DashboardLayoutProps = {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="flex min-h-dvh">
-      <Sidebar />
-      <main className="flex-1 overflow-y-auto h-full">{children}</main>
+      <main className="flex-1 overflow-y-auto h-full lg:pl-[280px]">
+        {children}
+      </main>
     </div>
   );
 }
